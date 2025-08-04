f829b70ff7e0078bb04b8642bec6e2245bcdf2cf
Khushi Rai <155308071+Khushicoder04@users.noreply.github.com>
2025-08-04 13:37:27 +0000

    datatyoes

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,19 @@
+const accId = 223242 // we can not change
+let accEmail = "khushi@gmail.com"
+var accPass = "1234"
+accCity = "Hyd"
+let accState;// it will give undefined
+// not use const because state changes
+
+//accId = 2; // not allowed
+console.log(accId);
+
+accEmail = "ac@gmail.com"
+accPass = "3232323"
+accCity = "pune"
+
+console.table([accId, accEmail, accPass, accCity, accState])
+/*
+prefer not use var because of issue in block scope 
+and functional scope
+*/
---
+++ .devcontainer/01_basic/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") // we can not use like this ..... we are using nodejs not browser
+
+console.log(3 
+    + 3) // do not do like this, code readability should be high
+
+    // documentation --> tc39.es original docum of js
+
+    let name = "khushi"
+    let age = 18
+    let isLoggedIn = false
+    // number => 2 to power 53
+    //bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined
+    // symbol => to find the uniqueness
+
+
+    // object
+
+console.log(typeof "khushi"); 
+console.log(typeof null); // give object
+console.log(typeof undefined); // undefined
+
