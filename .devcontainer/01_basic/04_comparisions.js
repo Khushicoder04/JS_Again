0fbedc66459205341bbc418c0c0a0e6e961c5b03
Khushi Rai <155308071+Khushicoder04@users.noreply.github.com>
2025-08-05 13:23:30 +0000

    Comparison

---
+++ .devcontainer/01_basic/04_comparisions.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+
+//console.log("20" > 1);
+
+console.log(null > 0);
+console.log(null == 0);// comparision
+console.log(null >= 0);
+
+console.log(undefined == 0);
+
+// === strict check
+console.log("2" == 2);// true
+console.log("2" === 2); //false 
+
+
