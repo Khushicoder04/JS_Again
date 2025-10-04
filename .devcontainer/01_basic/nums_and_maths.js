8c7a219b705cd6b5bd16f78819cca45a2704adf5
Khushi Rai <155308071+Khushicoder04@users.noreply.github.com>
2025-10-04 07:27:36 +0000

    NumsAndMaths

--- .devcontainer/01_basic/nums_and_maths.js
+++ .devcontainer/01_basic/nums_and_maths.js
@@ -7,3 +7,10 @@ console.log(balance);
 console.log(balance.toString().length);
 console.log(balance.toFixed(1));
 
+console.log(Math.random()); //give value between 0 t0 1
+console.log(Math.floor(Math.random() * 10) + 1); // give value from 1 t0 9
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
