35ef16bbc00a2aab62e7909ec8359793dc028b0e
Khushi Rai <155308071+Khushicoder04@users.noreply.github.com>
2025-10-08 13:24:12 +0000

    Scopes

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -10,7 +10,7 @@ if(true){   //block scope outside global scope
     
     let a = 10
     const b = 20
-console.log("INNER: ", a);
+//console.log("INNER: ", a);
 
 }
 
@@ -19,6 +19,45 @@ console.log("INNER: ", a);
     
 // }
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+//nested scope
+function one(){
+    const username = "Khushi"
+
+    function two(){
+        const website = "youtube"
+        //console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+one()
+
+if(true){
+    const username = "khushi"
+    if(username == "khushi"){
+        const website = "youtube"
+        //console.log(username + website);
+        
+    }
+    //console.log(website);
+    
+}
+//console.log(username);
+
+
+//++++++++++++++++interesting++++++++++++++++++
+
+function addone(num){
+  return num + 1
+}
+addone(5)
+
+const addtwo = function(num){
+    return num + 2
+}
+addtwo(5)
