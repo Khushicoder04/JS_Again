9d0189c092a7df06064ce3a75396b95115a762d1
Khushi Rai <155308071+Khushicoder04@users.noreply.github.com>
2025-10-16 13:06:19 +0000

    whileLoop

--- .devcontainer/05_iterations/two.js
+++ .devcontainer/05_iterations/two.js
@@ -1,3 +1,22 @@
-while (condition) {
-    
-}
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`value of index ${index}`);
+//     index += 2;
+// }
+
+// let myArr = ['flash', 'batman', 'superman']
+
+// let arr =  0
+// while(arr < myArr.length){
+//     console.log(`value is ${myArr[arr]}`);
+//     arr += 1;
+// }
+
+
+
+let score = 1;
+do {
+console.log(`Score is ${score}`);
+score++;
+} while (score <= 10);
